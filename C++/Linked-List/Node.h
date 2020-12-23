8fa16cdcb8ce86a767f7eb47bccbf41bcb120948
huyleminh <leminhhuy.hcmus@gmail.com>
2020-12-23 15:52:10 +0700

    commit: Singly linked list

---
+++ C++/Linked-List/Node.h
@@ -0,0 +1,66 @@
+#ifndef _NODE_H_
+#define _NODE_H_
+
+template<class T>
+class Node
+{
+protected:
+    T _key;
+    Node* _next;
+public:
+    Node() : _next(nullptr) {}
+    Node(const T& key) : _key(key), _next(nullptr) {}
+
+    Node*& operator =(Node* p);
+
+    T& getKey();
+    void setKey(const T& key);
+    Node*& next();
+
+    static Node* init(const T& key);
+
+    ~Node();
+};
+
+template<class T>
+Node<T>*& Node<T>::operator =(Node* p) {
+    if (!p) {
+        Node<T>::Node();
+        return *this;
+    }
+    this->~Node();
+    this->_key = p->_key;
+    this->_next = p->_next;
+    return *this;
+}
+
+template<class T>
+T& Node<T>::getKey() {
+    return this->_key;
+}
+
+template<class T>
+void Node<T>::setKey(const T& key) {
+    this->_key = key;
+}
+
+template<class T>
+Node<T>*& Node<T>::next() {
+    return this->_next;
+}
+
+template<class T>
+Node<T>* Node<T>::init(const T& key) {
+    Node<T>*& p = new Node(key);
+    return p;
+}
+
+template<class T>
+Node<T>::~Node() {
+    if (!_next)
+        return;
+    this->next()->~Node();
+    delete _next;
+}
+
+#endif
