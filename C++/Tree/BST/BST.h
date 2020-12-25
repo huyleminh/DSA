bf59ac0d0aec6fe4b3319ddade51673d89f2191e
huyleminh <leminhhuy.hcmus@gmail.com>
2020-12-25 23:29:42 +0700

    commit: Spilt file

--- C++/Tree/BST/BST.h
+++ C++/Tree/BST/BST.h
@@ -17,8 +17,6 @@
 #define _BTS_H_
 
 #include "BSTNode.h"
-#include <iostream>
-using namespace std;
 
 template<class T>
 class BST {
@@ -76,109 +74,9 @@ public:
     }
 };
 
-//------------------------------------------------
-//private
 
-template<class T>
-void BST<T>::preOrderTraversal(BSTNode<T>* root) {
-    if (!root)
-        return;
-    cout << root->getKey() << " ";
-    preOrderTraversal(root->left());
-    preOrderTraversal(root->right());
-}
-
-template<class T>
-void BST<T>::inOrderTraversal(BSTNode<T>* root) {
-    if (!root)
-        return;
-    inOrderTraversal(root->left());
-    cout << root->getKey() << " ";
-    inOrderTraversal(root->right());
-}
-
-template<class T>
-void BST<T>::posOrderTraversal(BSTNode<T>* root) {
-    if (!root)
-        return;
-    posOrderTraversal(root->left());
-    posOrderTraversal(root->right());
-    cout << root->getKey() << " ";
-}
-
-template<class T>
-BSTNode<T>* BST<T>::search(BSTNode<T>* root, const T& key) {
-    if (!_root)
-        return nullptr;
-
-    BSTNode<T>* p = root;
-    while (p) {
-        if (p->getKey() == key)
-            return p;
-        p = (key < p->getKey()) ? p->left() : p->right();
-    }
-    return nullptr;
-}
-
-template<class T>
-void BST<T>::insert(BSTNode<T>*& root, const T& key) {
-    if (!root) {
-        root = new BSTNode<T>(key);
-        return;
-    }
-    if (root->getKey() == key)
-        return;
-    else if (root->getKey() > key)
-        insert(root->left(), key);
-    else 
-        insert(root->right(), key);
-}
-
-template<class T>
-void BST<T>::removeNode(BSTNode<T>*& root, const T& key) {
-    if (!root)
-        return;
-    
-    if (root->getKey() > key)
-        removeNode(root->left(), key);
-    else if (root->getKey() < key)
-        removeNode(root->right(), key);
-    else {
-        if (!root->left() && !root->right()) {
-            delete root;
-            root = nullptr;
-        }
-        else if (!root->left() || root->right()) {
-            BSTNode<T>* p = root;
-            root = (root->left()) ? root->left() : root->right();
-            delete p;
-        }
-        else {
-            BSTNode<T>* predecessorNode = this->predecessor(root->left());
-            root->setKey(predecessorNode->getKey());
-            removeNode(root->left(), predecessorNode->getKey());
-        }
-    }
-}
-
-template<class T>
-BSTNode<T>* BST<T>::predecessor(BSTNode<T>* root) {
-    if (!root)
-        return nullptr;
-    BSTNode<T>* p = root;
-    while (p->right())
-        p = p->right();
-    return p;
-}
-
-template<class T>
-void BST<T>::clear(BSTNode<T>*& root) {
-    if (!root)
-        return;
-    clear(root->left());
-    clear(root->right());
-    delete root;
-}
-
+#ifndef _BST_CPP_
+#include "BST.cpp"
+#endif
 
 #endif
