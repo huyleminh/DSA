4772028dd782ab19ede3376a84ffd0c2385b3c56
huyleminh <leminhhuy.hcmus@gmail.com>
2020-12-31 03:29:48 +0700

    commit: fix search and add index.ts

---
+++ TypeScript/src/index.ts
@@ -0,0 +1,7 @@
+import Sort from "./sort/Sort";
+import Search from "./search/Search";
+
+let arr = [5, 7, 0, 1, 3, 4, 9, 2, 6]
+
+Sort.radixSort(arr);
+console.log(arr);
--- TypeScript/src/search/Search.ts
+++ TypeScript/src/search/Search.ts
@@ -12,40 +12,56 @@
 */
 
 
-//Linear search
-function linearSearch(arr: Array<number>, key: number): number {
-    for (let i = 0; i < arr.length; i++)
-        if (arr[i] === key)
-            return i;
-    return -1;
-}
+export default class Search {
 
-//Binary search
-function binarySearch(arr: Array<number>, key: number): number {
-    let left = 0;
-    let right = arr.length - 1;
+    /**
+     * @param arr: array list
+     * @param key: key to search
+     * @return index founded or -1
+     */
+    public static linearSearch<T> (arr: Array<T>, key: T): number {
+        for (let i = 0; i < arr.length; i++)
+            if (arr[i] === key)
+                return i;
+        return -1;
+    }
 
-    while(left < right) {
+    /**
+     * @param arr: array list
+     * @param key: key to search
+     * @return index founded or -1
+     */
+    public static binarySearch<T> (arr: Array<T>, key: T): number {
+        let left = 0;
+        let right = arr.length - 1;
+
+        while(left < right) {
+            let mid = Math.floor((left + right) / 2);
+            if (arr[mid] === key)
+                return mid;
+            if (arr[mid] < key)
+                left = mid + 1;
+            else 
+                right = mid - 1;
+        }
+        if (left == right)
+            return (arr[left] === key) ? left : -1;
+        return -1;
+    }
+
+    /**
+     * @param arr: array list
+     * @param key: key to search
+     * @param left: left index
+     * @param right: right index
+     * @return index founded or -1
+     */
+    public static recursiveBinarySearch<T> (arr: Array<T>, key: T, left: number, right: number): number {
+        if (right < left)
+            return -1;
         let mid = Math.floor((left + right) / 2);
         if (arr[mid] === key)
             return mid;
-        if (arr[mid] < key)
-            left = mid + 1;
-        else 
-            right = mid - 1;
+        return (arr[mid] > key) ? this.recursiveBinarySearch(arr, key, left, mid - 1) : this.recursiveBinarySearch(arr, key, mid + 1, right);
     }
-    if (left == right)
-        return (arr[left] === key) ? left : -1;
-    return -1;
 }
-
-function recursiveBinarySearch(arr: Array<number>, key: number, left: number, right: number): number {
-    if (right < left)
-        return -1;
-    let mid = Math.floor((left + right) / 2);
-    if (arr[mid] === key)
-        return mid;
-    return (arr[mid] > key) ? recursiveBinarySearch(arr, key, left, mid - 1) : recursiveBinarySearch(arr, key, mid + 1, right);
-}
-
-export {linearSearch ,binarySearch, recursiveBinarySearch};
