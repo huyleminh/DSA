82440abee239ca8c574312df4c73326d691e5340
huyleminh <leminhhuy.hcmus@gmail.com>
2020-12-31 14:48:07 +0700

    index.js

--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -1,7 +1,12 @@
 import Sort from "./sort/Sort";
 import Search from "./search/Search";
+import Stack from "./stack/Stack"
 
-let arr = [5, 7, 0, 1, 3, 4, 9, 2, 6]
+// let arr = [5, 7, 0, 1, 3, 4, 9, 2, 6]
 
-Sort.radixSort(arr);
-console.log(arr);
+// Sort.radixSort(arr);
+// console.log(arr);
+
+
+let st = new Stack<number[]>();
+st.push([1], [2], [3, 4, 5]);
